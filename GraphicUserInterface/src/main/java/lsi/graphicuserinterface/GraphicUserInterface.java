fd062bd54b68c6258c033745d5aab085072c9ba6
Luigi <124099993+AndroLuix@users.noreply.github.com>
2023-03-12 12:37:56 +0100

    Add files via upload

---
+++ GraphicUserInterface/src/main/java/lsi/graphicuserinterface/GraphicUserInterface.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package lsi.graphicuserinterface;
+
+/**
+ *
+ * @author lui12
+ */
+public class GraphicUserInterface {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
